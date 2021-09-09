4e097cb31c6c6d360317c08d7fc0c6b98a5161e0
christophersiem <christopher.siem@gmail.com>
2021-09-09 14:49:59 +0200

    Add threatlevel

--- src/main/java/Alarm.java
+++ src/main/java/Alarm.java
@@ -1,8 +1,24 @@
 public class Alarm {
-    public static String getCapacityStatus(int numberOfPeople) {
-        if (numberOfPeople > 30) {
-            return "Warning! More than 30 people are not allowed";
+    public static String getCapacityStatus(int numberOfPeople, String threatLevel) {
+
+        int maxNumberOfPeopleAllowed = getMaximumNumberOfPeopleAllowed(threatLevel);
+
+        if (numberOfPeople > maxNumberOfPeopleAllowed) {
+            return "Warning! Maximum number of people exceeded";
         }
         return "Everything okay, maximum number of people not exceeded";
     }
+
+    public static int getMaximumNumberOfPeopleAllowed(String threatLevel){
+        switch(threatLevel){
+            case "green":
+                return 60;
+            case "yellow":
+                return 30;
+            default :
+                 return 0;
+        }
+    }
+
+
 }
--- src/test/java/AlarmTest.java
+++ src/test/java/AlarmTest.java
@@ -1,33 +1,24 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AlarmTest {
 
-    @Test
-    @DisplayName("More than 30 Persons should return a warning")
-    public void testGetCapacityStatusWarning() {
-        //Given
-        int numberOfPeople = 31;
+    @ParameterizedTest
+    @CsvSource({
+            "red, 30, Warning! Maximum number of people exceeded",
+            "red, 0, 'Everything okay, maximum number of people not exceeded'",
+            "yellow, 29, 'Everything okay, maximum number of people not exceeded'",
+            "yellow, 31, Warning! Maximum number of people exceeded",
+            "green, 59, 'Everything okay, maximum number of people not exceeded'",
+            "green, 61, Warning! Maximum number of people exceeded",
 
+    })
+    public void checkAllowedNumberOfPersons(String threatLevel, int numberOfPeople, String expected){
         //When
-        String actual = Alarm.getCapacityStatus(numberOfPeople);
-
+        String actual = Alarm.getCapacityStatus(numberOfPeople, threatLevel);
         //Then
-        assertEquals("Warning! More than 30 people are not allowed", actual);
-    }
-
-    @Test
-    @DisplayName("Less than 31 Persons should return a OK message")
-    public void testGetCapacityStatusOk() {
-        //Given
-        int numberOfPeople = 30;
-
-        //When
-        String actual = Alarm.getCapacityStatus(numberOfPeople);
-
-        //Then
-        assertEquals("Everything okay, maximum number of people not exceeded", actual);
+        assertEquals(expected, actual);
     }
 }
